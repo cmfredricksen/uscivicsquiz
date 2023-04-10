f3b4767fda6a725d7ae621785130560bed47f5c3
Chris Fredricksen <cmfredricksen@gmail.com>
2023-04-10 12:14:40 -0500

    add app.css, layout, fonts, basic formatting

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="wrapper">
+	<slot><!-- optional fallback --></slot>
+</div>
+
+<style>
+	.wrapper {
+		width: 80vw;
+		margin: 0 auto;
+	}
+</style>
